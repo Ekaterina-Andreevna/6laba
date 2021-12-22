69a483e0b9912f20f5de3650633380940e54b9c3
honor <honor@LAPTOP-SGLNFRHD>
2021-12-22 16:16:32 +0300

    Добавьте файлы проекта.

---
+++ 6laba/6laba.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+using namespace std;
+
+
+/*22.Дана квадратная целочисленная матрица А(nn).
+Найти суммы элементов строк, имеющих четные элементы на главной диагонали.*/
+
+
+int main()
+	{
+		srand(time(NULL));
+
+		int n = 0;
+	
+		cout <<  "vvedite znachenie storon kvadrata" << endl;
+		cin >> n;    
+			
+		int** a = new int* [n]; 
+		for (int i = 0; i < n; i++)
+		{
+			a[i] = new int[n]; 
+		}
+		
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				a[i][j] = rand() % 10; 
+				cout << a[i][j] << " "; 
+			}
+			cout << endl; 
+		}
+		
+	
+
+	int gs = 0; // будет считать суммы элементов строк, где элемент на глав диагонали чётный
+	int l = 0;  // отслеживания "уровней"
+
+	for (int i = 0; i < n; i++) {
+
+		int cs = 0; // будет считать сумму элементов в строке
+
+		for (int j = 0; j < n; j++) {
+			cs += a[i][j];
+		}
+
+		if (a[l][l] % 2 == 0) {
+			gs += cs;
+		}
+
+		l += 1;
+	}
+
+	cout << gs;
+
+	return 0;
+    
+
+
+
+	for (int i = 0; i < n; i++)
+	{
+		delete[]a[i]; // удаление каждого элемента
+	}
+	delete[] a; // удаление массива
+	return 0;
+
+}
